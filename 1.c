cee92722a1c63627f785cf7fc1a4c4d49398644a
Mickeyyayaya <43983862+Mickeyyayaya@users.noreply.github.com>
2019-11-14 15:03:50 +0800

    Create 1.c

---
+++ 1.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+
+int main()
+{
+    int price = 8;
+    int gram;
+    scanf("%d",&gram);
+    if(gram > 1000)
+    {
+        gram = gram % 1000 ;
+        gram = gram / 500 +1 ;
+        price = price + gram * 4;
+    }
+
+    int fast;
+    scanf("%d",&fast);
+    if(fast == 1)
+        price = price +5;
+    else if(fast == 2)
+        price = price + 10;
+
+    printf("%d",price);
+    return 0;
+}
